01cb4e87fedcd322d5441a3385cf12b1458e8a70
shivg <docshivaguru@gmail.com>
2016-01-13 15:30:19 -0800

    Set-up skeleton for pseudo-classical queue class

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.queueSize = 0;
 };
+
+Queue.prototype.enqueue = function() {};
+
+Queue.prototype.dequeue = function() {};
+
+Queue.prototype.size = function() {};
 
 
