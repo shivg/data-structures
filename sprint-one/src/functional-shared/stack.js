5eb2f0a61d2102430c37def0c4bdb98f42f472b8
shivg <docshivaguru@gmail.com>
2016-01-13 14:52:53 -0800

    Started skeleton for function-shared stack class

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  var stackSize = 0;
+  var stackInstance = {};
+  return _.extends(stackInstance, stackMethods);
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function() {},
+  pop: function() {},
+  size: function() {}
+
+
+};
 
 
