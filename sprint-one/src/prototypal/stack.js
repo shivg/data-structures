1b56fd06a86e5c7b7d5158983a55b1c9012e6874
shivg <docshivaguru@gmail.com>
2016-01-13 15:22:13 -0800

    Set-up skeleton for prototypal stack class

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = Object.create(stackMethods);
+  stackInstance.stackSize = 0;
+  stackInstance.storage = {};
+
+  return stackInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function (value) {
+    // body...
+  },
+  pop : function () {
+    // body...
+  },
+  size : function () {
+    // body...
+  }
+};
 
 
