7ac5120c5fd8be759fa53a0f3d7614c127b3e53a
shivg <docshivaguru@gmail.com>
2016-01-13 15:02:26 -0800

    Set-up basic skeleton for functional-shared queue class

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueInstance = {
+    queueSize : 0,
+    storage : {}
+  };
+  return _.extend(queueInstance, queueMethods);
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function (value) {
+      
+  },
+  dequeue : function () {
+    // body...
+  },
+  size : function () {
+  
+  }
+};
 
 
