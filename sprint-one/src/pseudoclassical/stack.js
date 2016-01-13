d2f8eb16048d6a86f09965496455fcbda3b6944b
shivg <docshivaguru@gmail.com>
2016-01-13 15:37:43 -0800

    Set-up skeleton for pseudoclassical version of stack class

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  this.storage = {};
 };
 
+Stack.prototype.pop = function () {
+  // body...
+};
+
+Stack.prototype.push = function () {
+  // body...
+};
+
+Stack.prototype.size = function () {
+  // body...
+};
 
