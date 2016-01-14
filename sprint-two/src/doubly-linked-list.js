57edc0d9ae205bcae78366388fa2945386f8facf
shivg <docshivaguru@gmail.com>
2016-01-14 10:41:23 -0800

    Created new file for doubly-linked-list class and pasted LinkedList template inside

---
+++ sprint-two/src/doubly-linked-list.js
@@ -0,0 +1,53 @@
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else if(this.tail == null) {
+      this.tail = newNode;
+      this.head.next = this.tail;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  
+    
+  };
+
+  list.removeHead = function() {
+    var result = this.head.value;
+    this.head = this.head.next;
+    return result;
+  };
+
+  list.contains = function(target) {
+    var found = false;
+    var examinedNode = this.head;
+    while(examinedNode !== null){
+      if (examinedNode.value === target) {
+        found = true;
+        break;
+      }
+      examinedNode = examinedNode.next;
+    }
+
+
+    return found;
+  };
+
+  return list;
+};
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
