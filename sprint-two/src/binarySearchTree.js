0adc3652e15d0532094ed51d10cbf8006a0c547d
shivg <docshivaguru@gmail.com>
2016-01-14 10:12:02 -0800

    Setup skeleton for binarySearchTree Class

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,22 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ BinarySearchTree.prototype.insert = function (value) {
+   // body...
+ };
+BinarySearchTree.prototype.contains = function () {
+  // body...
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+  // body...
+};
