4f5f918942c4f82b1d878e34790dfdf9d3231a94
shivg <docshivaguru@gmail.com>
2016-01-14 10:22:29 -0800

    Added insert to BST class and re-implemented using prototypal chain instantiation

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,8 +1,9 @@
 var BinarySearchTree = function(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
-
+  var BSTInstance = Object.create(BinarySearchTree.prototype);
+  BSTInstance.value = value;
+  BSTInstance.left = null;
+  BSTInstance.right = null;
+  return BSTInstance;
 };
 
 
@@ -12,6 +13,14 @@ var BinarySearchTree = function(value) {
 
  BinarySearchTree.prototype.insert = function (value) {
    // body...
+  var newBinaryTree = new BinarySearchTree(value);
+  if(value < this.value) { //look left
+    if(this.left === null) this.left = newBinaryTree;
+    else this.left.insert(value);
+  } else { //lookright
+    if(this.right === null) this.right = newBinaryTree;
+    else this.right.insert(value);
+  }
  };
 BinarySearchTree.prototype.contains = function () {
   // body...
