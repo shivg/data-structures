05f28c28554dd7be396be7c2877580c24d2ec8b2
shivg <docshivaguru@gmail.com>
2016-01-13 21:03:29 -0800

    Re-adjusted storage of keyValue Pair in hashTable class

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,18 +7,18 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  this._storage.set(index, [k,v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  return this._storage.get(index)[1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function(value, key, collection) {
-    if(key === index) collection.splice(index,1);
+  this._storage.each(function(keyValuePair, hashIndex, collection) {
+    if(hashIndex === index) collection.splice(index,1);
   });
 };
 
