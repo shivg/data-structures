2ba69760200d2f67d3f4046c9fbb0cb81f1739b8
shivg <docshivaguru@gmail.com>
2016-01-13 20:24:52 -0800

    Implemented Set Class

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,16 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(this._storage === undefined) this._storage = {};
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return item in this._storage 
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
