89b7609bc065ce157a601b2fbdf20acc82b91ad2
shivg <docshivaguru@gmail.com>
2016-01-15 14:42:50 -0800

    Added RunTrials function for the BloomFilterSpec

--- sprint-two/spec/bloomFilterSpec.js
+++ sprint-two/spec/bloomFilterSpec.js
@@ -1,17 +1,13 @@
 describe('bloomFilter', function () {
   // body..
-  var users;
-  var BFIntance;
-  var notAdded;
-  beforeEach(function () {
-    // body...
-    users = ["Orlando", "Shiva", "Andrew", "Brendan", "Dave"];
-    notAdded = ["Shannon", "Sarah", "Claire", "Vince", "Lance"];
-    BFIntance = new BloomFilter(18);
-  });
+  
+  
+  var users = ["Orlando", "Shiva", "Andrew", "Brendan", "Dave"];
+  var notAdded = ["Shannon", "Sarah", "Claire", "Vince", "Lance"];
+  var BFIntance = new BloomFilter(18);
+
 
   it("Should Store and Check for items",function () {
-    // body...
     _.each(users, function(user) {
       BFIntance.store(user);
     });
@@ -21,4 +17,32 @@ describe('bloomFilter', function () {
   it("Should not find items that were not stored", function(){
     expect(_.every(notAdded, user => !BFIntance.check(user))).to.equal(true);
   });
+
+  it("Should run trial cases", function(){});
+  
+
+
 });
+
+
+
+function runTrials() {
+  // body...
+  var fail = 0;
+  var users = ["Orlando", "Shiva", "Andrew", "Brendan", "Dave"];
+  var BFIntance = new BloomFilter(18);
+
+  _.each(users, function (user) {
+      BFIntance.store('user');
+    });
+
+  for(var i = 0 ; i<10000 ; i++){
+    var str = "";
+    for(var j = 0; j < 6; j++) {
+      str += String.fromCharCode(Math.random()*26 + 97)
+    }
+    if(BFIntance.check(str)) fail++;
+  }
+
+  return fail/10000;
+}
