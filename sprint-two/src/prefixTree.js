45041ad9332c058f36ead34c3a6ae25884d1fd1e
shivg <docshivaguru@gmail.com>
2016-01-15 15:54:37 -0800

    Implemented insert method of preFix Tree class

--- sprint-two/src/prefixTree.js
+++ sprint-two/src/prefixTree.js
@@ -1,14 +1,31 @@
 var PrefixTree = function (value) {
   this.value = value || "";
   this.children = [];
+  this.parent = null;
 };
 
 
-PrefixTree.prototype.insert = function (string) {
-  
+PrefixTree.prototype.insert = function (str) {
+  var newPT;
+  if(str === '') {
+    return;
+  } else if(this.children.length === 0) {
+    newPT = new PrefixTree(str[0]);
+    newPT.insert(str.slice(1, str.length));
+    this.children.push(newPT);
+  } else {
+    var matchingChild = this.children.find(function (childPT) {
+      return childPT.value === str[0];
+    });
+    if(matchingChild !== undefined) {
+      matchingChild.insert(str.slice(1, str.length));
+    } else {
+      newPT = new PrefixTree(str[0]);
+      newPT.insert(str.slice(1, str.length));
+      this.children.push(newPT);
+    }    
+  }
 };
-
-
 
 
 
