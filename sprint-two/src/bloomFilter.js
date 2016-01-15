45fc6af3da241822ef9ba3e7c57f7ca5af4d7cfb
shivg <docshivaguru@gmail.com>
2016-01-15 11:22:17 -0800

    Created check and store methods for BloomFilter class

--- sprint-two/src/bloomFilter.js
+++ sprint-two/src/bloomFilter.js
@@ -5,13 +5,23 @@ function BloomFilter (m) {
 }
 
 BloomFilter.prototype.check = function(value) {
+  var index1 = hash1(value) % this.m;
+  var index2 = hash2(value) % this.m;
+  var index3 = hash3(value) % this.m;
 
-
+  return index1 && index2 && index3;
 };
 
 BloomFilter.prototype.store = function(value) {
-
-
+  var getIndices = function(hashFunctions, value) {
+    _.map(hashFunctions, function (fn) {
+      return fn(value) % this.m;
+    });
+  };
+  var indices = getIndices([hash1, hash2, hash3], value);
+  _.each(indices, function(index) {
+    this.storage[index] = true;
+  });
 };
 
 
