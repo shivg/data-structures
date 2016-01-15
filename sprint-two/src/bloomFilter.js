62b51967b03afb2c9fb754ec28e91aa7f7d8f7fd
shivg <docshivaguru@gmail.com>
2016-01-15 11:12:26 -0800

    Setup skeleton for BloomFilter Class

---
+++ sprint-two/src/bloomFilter.js
@@ -0,0 +1,29 @@
+function BloomFilter (m) {
+  this.m = m;
+  this.storage = Array(m);
+
+}
+
+BloomFilter.prototype.check = function(value) {
+
+
+};
+
+BloomFilter.prototype.store = function(value) {
+
+
+};
+
+
+var hash3 = function (value) {
+  
+    // body...
+};
+
+var hash2 = function (value) {
+    // body...
+};
+
+var hash1 = function (value) {
+    // body...
+};
