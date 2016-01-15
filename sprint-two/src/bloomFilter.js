0d210d6df03da24a20c0f5a1ccf996aeeb1b2970
shivg <docshivaguru@gmail.com>
2016-01-15 11:39:35 -0800

    Created 3 random hash functions and added bloomFilter file to SpecRunner

--- sprint-two/src/bloomFilter.js
+++ sprint-two/src/bloomFilter.js
@@ -26,14 +26,32 @@ BloomFilter.prototype.store = function(value) {
 
 
 var hash3 = function (value) {
-  
-    // body...
+  value = JSON.stringify(value);
+  var hash = 1;
+  for(var i = 0; i < value.length; i++) {
+    hash = hash * value.charCodeAt(i);
+  }
+  return hash;
 };
 
 var hash2 = function (value) {
-    // body...
+  value = JSON.stringify(value);
+  var hash = 42;
+  for(var i = 0; i < value.length; i = i+2){
+    hash = Math.floor(value.charCodeAt(i)/2) + value.charCodeAt(i+1);
+  }
+  return hash;
+    // body...  
 };
 
 var hash1 = function (value) {
+  value = JSON.stringify(value);
     // body...
+  var hash = 6;
+  for(var i = 0; i< value.length ; i++){
+    hash = (hash << value.charCodeAt(i)) + i;
+  }
+  if (hash < 0 ) hash = ~hash;
+  return hash;
 };
+
