d0ac08cf9036264de332d2b26caaa23c01a73f44
shivg <docshivaguru@gmail.com>
2016-01-15 12:17:43 -0800

    Added testSpec to ensure BloomFilter can find stored items

--- sprint-two/spec/bloomFilterSpec.js
+++ sprint-two/spec/bloomFilterSpec.js
@@ -1,10 +1,19 @@
 describe('bloomFilter', function () {
-  // body...
+  // body..
+  var users;
+  var BFIntance;
   beforeEach(function () {
     // body...
+    users = ["Orlando", "Shiva", "Andrew", "Brendan", "Dave",
+                 "Shannon", "Sarah", "Claire", "Vince", "Lance"];
+    BFIntance = new BloomFilter(18);
   });
 
-  it(function () {
+  it("Should Store and Check for items",function () {
     // body...
+    _.each(users, function(user) {
+      BFIntance.store(user);
+    });
+    expect(_.every(users, user => BFIntance.check(user))).to.equal(true);
   });
 });
