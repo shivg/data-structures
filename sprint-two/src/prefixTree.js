9d477eb796fc41c9f7bdda9abbcdfff96d524c9e
shivg <docshivaguru@gmail.com>
2016-01-15 16:07:42 -0800

    Implemented navigate function of PT class

--- sprint-two/src/prefixTree.js
+++ sprint-two/src/prefixTree.js
@@ -11,19 +11,35 @@ PrefixTree.prototype.insert = function (str) {
     return;
   } else if(this.children.length === 0) {
     newPT = new PrefixTree(str[0]);
-    newPT.insert(str.slice(1, str.length));
+    newPT.insert(str.slice(1));
     this.children.push(newPT);
   } else {
     var matchingChild = this.children.find(function (childPT) {
       return childPT.value === str[0];
     });
     if(matchingChild !== undefined) {
-      matchingChild.insert(str.slice(1, str.length));
+      matchingChild.insert(str.slice(1));
     } else {
       newPT = new PrefixTree(str[0]);
-      newPT.insert(str.slice(1, str.length));
+      newPT.insert(str.slice(1));
       this.children.push(newPT);
     }    
+  }
+};
+
+PrefixTree.prototype.navigate = function(str) {
+  if(str.length === 1 && str[0] === this.value) {
+    return this;
+  } else {
+    var matchingChild = this.children.find(function (childPT) {
+      return childPT.value === str[0];
+    });
+    if(matchingChild !== undefined) {
+      if(str.length === 1 ) return matchingChild.navigate(str);
+      else return matchingChild.navigate(str.slice(1));
+    } else {
+      return false;
+    }
   }
 };
 
