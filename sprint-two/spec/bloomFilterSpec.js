c485c3b743189d3b63453ada6439d3caeddc9aa3
shivg <docshivaguru@gmail.com>
2016-01-16 09:44:40 -0800

    Added line to specRunner to show false positive rate of BloomFilter

--- sprint-two/spec/bloomFilterSpec.js
+++ sprint-two/spec/bloomFilterSpec.js
@@ -1,32 +1,6 @@
 describe('bloomFilter', function () {
   // body..
-  
-  
-  var users = ["Orlando", "Shiva", "Andrew", "Brendan", "Dave"];
-  var notAdded = ["Shannon", "Sarah", "Claire", "Vince", "Lance"];
-  var BFIntance = new BloomFilter(18);
-
-
-  it("Should Store and Check for items",function () {
-    _.each(users, function(user) {
-      BFIntance.store(user);
-    });
-    expect(_.every(users, user => BFIntance.check(user))).to.equal(true);
-  });
-
-  it("Should not find items that were not stored", function(){
-    expect(_.every(notAdded, user => !BFIntance.check(user))).to.equal(true);
-  });
-
-  it("Should run trial cases", function(){});
-  
-
-
-});
-
-
-
-function runTrials() {
+  function runTrials() {
   // body...
   var fail = 0;
   var users = ["Orlando", "Shiva", "Andrew", "Brendan", "Dave"];
@@ -46,3 +20,28 @@ function runTrials() {
 
   return fail/10000;
 }
+  
+  var users = ["Orlando", "Shiva", "Andrew", "Brendan", "Dave"];
+  var notAdded = ["Shannon", "Sarah", "Claire", "Vince", "Lance"];
+  var BFIntance = new BloomFilter(18);
+
+
+  it("Should Store and Check for items",function () {
+    _.each(users, function(user) {
+      BFIntance.store(user);
+    });
+    expect(_.every(users, user => BFIntance.check(user))).to.equal(true);
+  });
+
+  it("Should not find items that were not stored", function(){
+    expect(_.every(notAdded, user => !BFIntance.check(user))).to.equal(true);
+  });
+  var falsePositive = runTrials();
+  it("Runing 10,000 trial cases yields a false postive rate of "
+     + (falsePositive * 100).toFixed(2) + "%", function(){});
+  
+
+});
+
+
+
